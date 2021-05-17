2b8016341bb0a9cc2fdabb15366d05ec9edc5aa2
Binbin Dong <binbin.dong@cern.ch>
2021-05-17 21:42:19 +0200

    initial script

---
+++ SamplePrep/sample.py
@@ -0,0 +1,18 @@
+import uproot as up
+import numpy as np
+import h5py
+import pandas as pd
+
+input_file = "path_to_file"
+tree = "Signal"
+
+'''
+do conversion
+'''
+
+output_variable1 
+
+outputfile = h5py.File('path_to_output','w')
+outputfile.create_dataset(outputvariable1, data='output_variable1')
+outputfile.close()
+
