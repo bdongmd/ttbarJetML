fba18f3f20e66fae8f5413429c50d1a0180e5795
Binbin Dong <binbin.dong1994@gmail.com>
2021-06-02 22:17:44 +0200

    minor change

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 
-def private_DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True, batch_size=3000):
+def private_DL1Model(InputShape, h_layers, lr=0.01, drops=None, batch_size=3000):
 	In = keras.layers.Input(shape=[InputShape,])
 	x = In
 	for i, h in enumerate(h_layers[:]):
 		x = keras.layers.Dense(h, activation="relu",kernel_initializer='glorot_uniform')(x)
-		x = keras.layers.Dropout(drops[i])(x, training=dropout)
+		x = keras.layers.Dropout(drops[i])(x)
 		x = keras.layers.BatchNormalization()(x)
 
-	predictions = keras.layers.Dense(3, activation='softmax', kernel_initializer='glorot_uniform')(x)
+	predictions = keras.layers.Dense(2, activation='softmax', kernel_initializer='glorot_uniform')(x)
 
 	model = keras.models.Model(inputs=In, outputs=predictions)
 
