26ac4d8eefb365020e9e34a51ae33b7ead2f1309
Binbin Dong <binbin.dong1994@gmail.com>
2021-06-21 23:14:02 +0200

    working version

--- SamplePrep/sample.py
+++ SamplePrep/sample.py
@@ -3,7 +3,7 @@ import numpy as np
 import h5py
 import pandas as pd
 from variable_mapping import mapping
-from keras.utils import np_utils
+#from keras.utils import np_utils
 import plot_library
 
 import argparse
@@ -22,23 +22,25 @@ var_list = list(mapping.keys())
 df_S = Signal.pandas.df(var_list)
 df_B = Background.pandas.df(var_list)
 
-df_S['label'] = pd.Series(np.ones(len(df_S), dtype=int), index=df_S.index)
-df_B['label'] = pd.Series(np.zeros(len(df_B), dtype=int), index=df_B.index)
-plot_library.variable_plotting(df_S, df_B)
+#df_S['label'] = pd.Series(np.ones(len(df_S), dtype=int), index=df_S.index)
+#df_B['label'] = pd.Series(np.zeros(len(df_B), dtype=int), index=df_B.index)
+#plot_library.variable_plotting(df_S, df_B)
 
-X_train = np.concatenate((df_B, df_S))
-y_train = np.concatenate((np.zeros(len(df_B)), np.zeros(len(df_S))))
-Y_train = np_utils.to_categorical(y_train, 2)
+X_train = np.concatenate((pd.DataFrame(df_B), pd.DataFrame(df_S)))
+Y_train = np.concatenate((np.zeros(len(df_B)), np.zeros(len(df_S))))
+#Y_train = np_utils.to_categorical(y_train, 2)
+
+print(X_train.shape)
 
 rng_state = np.random.get_state()
 np.random.shuffle(X_train)
 np.random.set_state(rng_state)
 np.random.shuffle(Y_train)
-#assert X_train.shape[0] == len(var_list)
+assert X_train.shape[1] == len(var_list)
 
 
 outputfile = h5py.File(args.outputfile,'w')
-outputfile.create_dataset(X_train, data='X_train', compression='gzip')
-outputfile.create_dataset(Y_train, data='Y_train', compression='gzip')
+outputfile.create_dataset('X_train', data=X_train, compression='gzip')
+outputfile.create_dataset('Y_train', data=Y_train, compression='gzip')
 outputfile.close()
 
--- SamplePrep/variable_mapping.py
+++ SamplePrep/variable_mapping.py
@@ -1,3 +1,4 @@
 mapping = {
-		"jet_pt_0": "jet_pt_0"
+		"jet_pt_0": "jet_pt_0",
+		"jet_pt_1": "jet_pt_1"
 }
