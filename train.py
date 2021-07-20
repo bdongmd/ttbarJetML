81c034e7b68eb81ffc49f2603c47dae41b247822
Binbin Dong <binbin.dong1994@gmail.com>
2021-07-20 17:06:44 +0200

    saved loss and acc

--- train.py
+++ train.py
@@ -15,9 +15,10 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-i', '--input_file', type=str,
 		default="input/MC16d_hybrid-training_sample-NN.h5",
 		help='Set name of preprocessed input training file')
-parser.add_argument('-v', '--validate_file', type=str,
-		default="input/MC16d_ttbar-test-validation_sample-NN.h5",
-		help='Set name of preprocessed validation file')
+
+parser.add_argument('-o', '--output', type=str,
+		default='output/model_predicted.h5',
+		help='set the output file')
 parser.add_argument('-b', '--batch_size', type=int,
 		default=3000,
 		help='Set batch size')
@@ -27,22 +28,25 @@ parser.add_argument('-e', '--epoch', type=int,
 
 args = parser.parse_args()
 h5f_train = h5py.File(args.input_file, 'r')
-h5f_test = h5py.File(args.validate_file, 'r')
 
-X_train = h5f_train['X_train'][:]
-Y_train = h5f_train['Y_train'][:]
-X_test = h5f_test['X_test'][:]
-Y_test = h5f_test['Y_test'][:]
+totalEvents = len(h5f_train['X_train'])
+trainEvents = int(0.8*totalEvents)
 
-InputShape=44
-h_layers=[72, 57, 60, 48, 36, 24, 12, 6]
+X_train = h5f_train['X_train'][:trainEvents]
+Y_train = h5f_train['Y_train'][:trainEvents]
+X_test = h5f_train['X_train'][trainEvents:]
+Y_test = h5f_train['Y_train'][trainEvents:]
+
+
+InputShape=31
+h_layers=[60, 30, 15, 8]
 lr = 0.005
-drops=[0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]
+drops=[0.1, 0.2, 0.2, 0.2]
 dropout=True
 batch_size = args.batch_size
 
-DL1model = model.private_DL1Model(InputShape=InputShape, h_layers=h_layers, lr=lr, drops=drops, dropout=dropout)
-DL1model.summary()
+Model = model.private_DL1Model(InputShape=InputShape, h_layers=h_layers, lr=lr, drops=drops, dropout=dropout)
+Model.summary()
 
 callbacks = [
     keras.callbacks.ModelCheckpoint(
@@ -51,11 +55,23 @@ callbacks = [
     )
 ]
 
-history = DL1model.fit(X_train, Y_train,
+history = Model.fit(X_train, Y_train,
                     batch_size = args.batch_size,
                     epochs = args.epoch,
                     validation_data=(X_test, Y_test),
                     callbacks=callbacks
                     )
 
-DL1model.save("models/DL1_hybrid_2M_b{}_e{}.h5".format(args.batch_size, args.epoch))
+Model.save("models/training_b{}_e{}.h5".format(args.batch_size, args.epoch))
+
+train_loss = history.history['loss']
+train_acc = history.history['accuracy']
+val_loss = history.history['val_loss']
+val_acc = history.history['val_accuracy']
+
+hf = h5py.File(args.output, 'w')
+hf.create_dataset('train_loss', data=train_loss)
+hf.create_dataset('train_acc', data=train_acc)
+hf.create_dataset('val_loss', data=val_loss)
+hf.create_dataset('val_acc', data=val_acc)
+hf.close()
