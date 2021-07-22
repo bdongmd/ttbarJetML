8273cdc0476a9854b5e07a191b3f072c3debe4d2
Binbin Dong <binbin.dong1994@gmail.com>
2021-07-22 19:51:38 +0200

    added script for evaluation

---
+++ evaluation.py
@@ -0,0 +1,37 @@
+import numpy as np
+from plottingFunctions import sigBkgEff
+import h5py
+import tensorflow as tf
+import argparse
+
+parser = argparse.ArgumentParser(
+    description = 'options for running evaluation'
+)
+parser.add_argument('-i', '--input_file',type=str,
+    help='file used to do evaluation'
+)
+parser.add_argument('-m', '--model_name',type=str,
+    help='trained model'
+)
+parser.add_argument('-o', '--output_file',type=str,
+    help='output file'
+)
+
+args = parser.parse_args()
+
+h5f_test = h5py.File(args.input_file, 'r')
+
+X_test = h5f_test['X_test'][:]
+Y_test = h5f_test['Y_test'][:]
+labels = h5f_test['labels'][:]
+
+h5f_test.close()
+
+test_model = tf.keras.models.load_model(args.model_name)
+output = test_model.predict(X_test, verbose=2)
+
+h5f = h5py.File(args.output_file, 'w')
+h5f.create_dataset('pb', data=output[:,0], compression='gzip')
+h5f.create_dataset('ps', data=output[:,1], compression='gzip')
+h5f.create_dataset('labels', data=labels, compression='gzip')
+h5f.close()
