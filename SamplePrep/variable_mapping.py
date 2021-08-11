748f617ad1bab4788011938f51ab323493e54c37
Binbin Dong <binbin.dong1994@gmail.com>
2021-08-11 23:26:25 +0200

    added more variables

--- SamplePrep/variable_mapping.py
+++ SamplePrep/variable_mapping.py
@@ -1,4 +1,10 @@
 mapping = {
 		"jet_pt_0": "jet_pt_0",
-		"jet_pt_1": "jet_pt_1"
+		"jet_pt_1": "jet_pt_1",
+		"jet_pt_2": "jet_pt_2",
+		"dR_jl_0":"dR_jl_0",
+		"dR_jl_1":"dR_jl_1",
+		"jet_eta_0":"jet_eta_0",
+		"jet_eta_1":"jet_eta_1",
+		"jet_eta_2":"jet_eta_2", 
 }
