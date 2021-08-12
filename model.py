e17f98e3b97c36e1b683250e9cf6bdcc9715b33b
Binbin Dong <binbin.dong1994@gmail.com>
2021-08-12 23:31:43 +0200

    updated model

--- model.py
+++ model.py
@@ -1,30 +1,33 @@
-import tensorflow as tf
-import tensorflow.keras as keras
+from keras.layers import BatchNormalization
+from keras.layers import Dense, Activation, Input, add
+from keras.models import Model
+from keras.optimizers import Adam
 import sys
 
 def private_DL1Model(InputShape, outputShape, h_layers, lr=0.01, drops=None, dropout=True):
-	In = keras.layers.Input(shape=[InputShape,])
+	In = Input(shape=[InputShape,])
 	x = In
 	for i, h in enumerate(h_layers[:]):
-		x = keras.layers.Dense(h, activation="relu",kernel_initializer='glorot_uniform')(x)
+		x = Dense(h, kernel_initializer='glorot_uniform')(x)
+		x = BatchNormalization()(x)
+		x = Activation('relu')(x)
 		if dropout:
 			x = keras.layers.Dropout(drops[i])(x)
-		x = keras.layers.BatchNormalization()(x)
 
 	if outputShape == 1:
-		predictions = keras.layers.Dense(outputShape, activation='sigmoid', kernel_initializer='glorot_uniform')(x)
+		predictions = keras.layers.Dense(outputShape, activation='sigmoid')(x)
 	elif outputShape == 2:
-		predictions = keras.layers.Dense(outputShape, activation='softmax', kernel_initializer='glorot_uniform')(x)
+		predictions = keras.layers.Dense(outputShape, activation='softmax')(x)
 	else:
 		print("ERROR: wrong output numbers. The number of output categories can only be 1 or 2.")
 		sys.exit()
 
-	model = keras.models.Model(inputs=In, outputs=predictions)
+	model = Model(inputs=In, outputs=predictions)
 
-	model_optimizer = keras.optimizers.Adam(lr=lr)
+	model_optimizer = Adam(lr=lr)
 
 	model.compile(
-			loss = 'categorical_crossentropy',
+			loss = 'binary_crossentropy',
 			optimizer=model_optimizer,
 			metrics=['accuracy'])
 
